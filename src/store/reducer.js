c662ac5b015065555e3f7d8ed37aed9b439487c4
NBVinay <vinaynb63@gmail.com>
2020-09-13 15:50:29 +0530

    reducer js newest change

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,19 +7,19 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     console.log("inside reducer")
-    if(action.type == 'SOURCE'){
+    if(action.type === 'SOURCE'){
         const tempState = {...state}
         tempState['databaseConfigSourceId'] = action.newId
         state = tempState
     }
 
-    if(action.type == 'TARGET'){
+    if(action.type === 'TARGET'){
         const tempState = {...state}
         tempState['databaseConfigTargetId'] = action.newId
         state = tempState
     }
 
-    if(action.type == 'ADD USER'){
+    if(action.type === 'ADD USER'){
         const tempState = {...state}
         tempState['currentUser'] = action.newUser
         tempState['databaseConfigSourceId'] = ''
